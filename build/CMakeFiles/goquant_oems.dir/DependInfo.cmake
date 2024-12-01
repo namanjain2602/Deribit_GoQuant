ad067672383cb03c479321523b968730934b5742
Naman <nj260602@gmail.com>
2024-12-01 18:20:10 +0530

    Deribit Quant Commit

---
+++ build/CMakeFiles/goquant_oems.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/naman/Deribit_GoQuant/GoQuantApp/api_credentials.cpp" "/home/naman/Deribit_GoQuant/build/CMakeFiles/goquant_oems.dir/GoQuantApp/api_credentials.cpp.o"
+  "/home/naman/Deribit_GoQuant/GoQuantApp/main.cpp" "/home/naman/Deribit_GoQuant/build/CMakeFiles/goquant_oems.dir/GoQuantApp/main.cpp.o"
+  "/home/naman/Deribit_GoQuant/GoQuantApp/order_manager.cpp" "/home/naman/Deribit_GoQuant/build/CMakeFiles/goquant_oems.dir/GoQuantApp/order_manager.cpp.o"
+  "/home/naman/Deribit_GoQuant/GoQuantApp/token_manager.cpp" "/home/naman/Deribit_GoQuant/build/CMakeFiles/goquant_oems.dir/GoQuantApp/token_manager.cpp.o"
+  "/home/naman/Deribit_GoQuant/GoQuantApp/utility_manager.cpp" "/home/naman/Deribit_GoQuant/build/CMakeFiles/goquant_oems.dir/GoQuantApp/utility_manager.cpp.o"
+  "/home/naman/Deribit_GoQuant/GoQuantApp/web_socket_client.cpp" "/home/naman/Deribit_GoQuant/build/CMakeFiles/goquant_oems.dir/GoQuantApp/web_socket_client.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vcpkg/installed/x64-linux/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/goquant_oems.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goquant_oems.dir/GoQuantApp/api_credentials.cpp.o"
+  "CMakeFiles/goquant_oems.dir/GoQuantApp/main.cpp.o"
+  "CMakeFiles/goquant_oems.dir/GoQuantApp/order_manager.cpp.o"
+  "CMakeFiles/goquant_oems.dir/GoQuantApp/token_manager.cpp.o"
+  "CMakeFiles/goquant_oems.dir/GoQuantApp/utility_manager.cpp.o"
+  "CMakeFiles/goquant_oems.dir/GoQuantApp/web_socket_client.cpp.o"
+  "goquant_oems"
+  "goquant_oems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goquant_oems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
